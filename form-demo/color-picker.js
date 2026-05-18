5a1ee6e99b560755a7278414f7ba8addaf871802
Mark Volkmann <r.mark.volkmann@gmail.com>
2026-05-18 10:33:04 -0500

    progress

---
+++ form-demo/color-picker.js
@@ -0,0 +1,59 @@
+import { css, html, Wrec } from "wrec";
+import "./number-slider";
+
+class ColorPicker extends Wrec {
+  static formAssociated = true;
+
+  static properties = {
+    labelWidth: { type: String, value: "3rem" },
+    red: { type: Number },
+    green: { type: Number },
+    blue: { type: Number },
+    color: {
+      type: String,
+      computed: "`rgb(${this.red}, ${this.green}, ${this.blue})`",
+    },
+  };
+
+  static css = css`
+    :host {
+      display: flex;
+      gap: 0.5rem;
+    }
+
+    #sliders {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+    }
+
+    #swatch {
+      background-color: this.color;
+      height: 5rem;
+      width: 5rem;
+    }
+  `;
+
+  static html = html`
+    <div id="swatch"></div>
+    <div id="sliders">
+      <!-- prettier-ignore -->
+      ${this.makeSlider("Red")}
+      ${this.makeSlider("Green")}
+      ${this.makeSlider("Blue")}
+    </div>
+  `;
+
+  static makeSlider(label) {
+    return html`
+      <number-slider
+        label=${label}
+        label-width="this.labelWidth"
+        max="255"
+        value="this.${label.toLowerCase()}"
+      ></number-slider>
+    `;
+  }
+}
+
+ColorPicker.define("color-picker");
---
+++ form-demo/number-slider.js
@@ -0,0 +1,44 @@
+import { css, html, Wrec } from "wrec";
+
+class NumberSlider extends Wrec {
+  static formAssociated = true;
+
+  static properties = {
+    label: { type: String },
+    labelWidth: { type: String },
+    max: { type: Number, value: 100 },
+    min: { type: Number, value: 0 },
+    value: { type: Number },
+  };
+
+  static css = css`
+    :host {
+      display: flex;
+      align-items: center;
+      gap: 0.5rem;
+    }
+
+    input[type="range"] {
+      width: 7rem;
+    }
+
+    label {
+      font-weight: bold;
+      text-align: right;
+      width: this.labelWidth;
+    }
+  `;
+
+  static html = html`
+    <label>this.label</label>
+    <input
+      type="range"
+      min="this.min"
+      max="this.max"
+      value:input="this.value"
+    />
+    <span>this.value</span>
+  `;
+}
+
+NumberSlider.define("number-slider");
